d4c34ea6fabc49105627428c24548d8ac8d7ba0b
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-04-07 15:16:48 +1000

    made so that old search results are removed before displaying new ones

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,11 @@ import { getBooksBySearch } from "./requests.js";
 
 const form = document.querySelector("form");
 const seachBar = document.querySelector("#searchBar");
+const bookGrid = document.getElementById("bookGrid");
 
 form.addEventListener("submit", (e) => {
     console.log("searching");
+    bookGrid.innerHTML = "";
     getBooksBySearch(seachBar.value);
     e.preventDefault();
     e.stopPropagation();
