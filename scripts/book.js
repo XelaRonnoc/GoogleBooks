f56377bedd82da200bbcc659fb0bb81f435894f4
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-04-09 11:31:15 +1000

    added module with increased description when clicked on book

--- scripts/book.js
+++ scripts/book.js
@@ -4,6 +4,10 @@ export class Book {
     author;
     title;
     description;
+    release;
+    publishDate;
+    country;
+    languages;
     constructor() {}
 
     render(grid) {
@@ -40,6 +44,48 @@ export class Book {
                     wordLimiter(this.description, 120)
                 ) +
                 "..."
+        );
+        description.appendChild(descText);
+
+        container.addEventListener("click", (e) => {
+            this.renderModule(document.querySelector("body"));
+            e.stopPropagation();
+        });
+    }
+
+    renderModule(body) {
+        const container = document.createElement("div");
+        container.classList.add("expanded");
+        body.appendChild(container);
+
+        const innerContainer = document.createElement("div");
+        innerContainer.classList.add("expanded__inner");
+        container.appendChild(innerContainer);
+
+        const image = document.createElement("img");
+        image.classList.add("inner__image");
+        this.image.thumbnail
+            ? (image.src = this.image.thumbnail)
+            : (image.src = "");
+        innerContainer.appendChild(image);
+
+        const title = document.createElement("p");
+        title.classList.add("inner__title");
+        const titleText = document.createTextNode(this.title);
+        title.appendChild(titleText);
+        innerContainer.appendChild(title);
+
+        const author = document.createElement("p");
+        author.classList.add("inner__author");
+        innerContainer.appendChild(author);
+        const authorText = document.createTextNode("Author: " + this.author);
+        author.appendChild(authorText);
+
+        const description = document.createElement("p");
+        description.classList.add("inner__description");
+        innerContainer.appendChild(description);
+        const descText = document.createTextNode(
+            "Description: " + this.description
         );
         description.appendChild(descText);
     }
--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,22 @@ import { getBooksBySearch } from "./requests.js";
 const form = document.querySelector("form");
 const seachBar = document.querySelector("#searchBar");
 const bookGrid = document.getElementById("bookGrid");
-
+const body = document.querySelector("body");
 form.addEventListener("submit", (e) => {
     console.log("searching");
     bookGrid.innerHTML = "";
     getBooksBySearch(seachBar.value);
     searchBar.value = "";
     e.preventDefault();
+    e.stopPropagation();
+});
+
+body.addEventListener("click", (e) => {
+    const childEls = body.children;
+    for (let i = 0; i < childEls.length; i++) {
+        if (childEls[i].className === "expanded") {
+            body.removeChild(childEls[i]);
+        }
+    }
     e.stopPropagation();
 });
